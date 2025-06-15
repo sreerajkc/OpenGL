4c9a8f6ce9d364d0325b85910548f3cc0807909f
sreerajkc <sreerajkc.dev@gmail.com>
2025-06-15 11:54:35 +0530

    implemented index buffers

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -115,23 +115,36 @@ int main(void)
 		std::cout << "Error!" << std::endl;
 	}
 
-	// Print the version of OpenGL
+	// Print the version of OpenGL	
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[6] =
+	float positions[] =
 	{
-		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		-0.5f, -0.5f,// 0
+		 0.5f, -0.5f,// 1
+		 0.5f,	0.5f,// 2
+
+		-0.5f,	0.5f,// 3
 	};
 
-	unsigned buffer;
-	glGenBuffers(1, &buffer);// creates buffer-objects names and store the names array address in buffer;
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	unsigned int indices[] =
+	{
+		0,1,2,
+		2,3,0
+	};
+
+	unsigned vbo;// vertex buffer object 
+	glGenBuffers(1, &vbo);// creates buffer-objects names and store the names array address in buffer;
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, 0);
+
+	unsigned ibo;// index buffer objects
+	glGenBuffers(1, &ibo);// creates buffer-objects names and store the names array address in buffer;
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader"); // path will be relative to the project working directory(Project > Properties > Debugging > Working Directory)
 	unsigned int shaderProgram = CreateShaderProgram(source.VertexSource, source.FragmentSource);
@@ -143,7 +156,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
